9df84705dc0481d5dbf93db25f0effbbbb3b85b7
JakubBansky <jakub.bansky@gmail.com>
2023-12-21 15:51:32 +0100

    2d list genHouse

--- main.js
+++ main.js
@@ -1,59 +1,27 @@
+console.log("loaded");
+
 let house = document.getElementById("house");
 
+function createGrid(grid) {
+    let house = document.getElementById("house");
 
-function initBoard(result, task){
-    var numId = 1;
-            for(let i = 0; i < result.size; i++){
-                let floor = document.createElement("div");
-                floor.classList.add("floor");
-                for(let j = 0; j < result.size; j++){
-                    let tile = document.createElement("div");
-                    tile.classList.add("tile");
-                    tile.setAttribute('id', numId);
-                    numId++;
-                    floor.appendChild(tile);
-                }
-                house.appendChild(floor);
+    grid.forEach(row => {
+        let floor = document.createElement("div");
+        floor.classList.add("floor");
+        row.forEach(tile => {
+            let item = document.createElement("div");
+            item.classList.add("tile");
+            if (tile == 1) {
+                item.classList.add("barrier");
+            } else if (tile == -1) {
+                item.classList.add("start");
             }
+            floor.appendChild(item);
+        })
+        house.appendChild(floor);
+    })
 
-            let start = document.getElementById(result.tasks[task].startPos);
-            start.classList.add("start");
-
-            for(let i = 0; i < result.tasks[task].barriers.length; i++){
-                let barrier = document.getElementById(result.tasks[task].barriers[i]);
-                barrier.classList.add("barrier");
-            }
 }
-
-function moveRight(currentIndex, size){
-    while (currentIndex < size && currentIndex) {
-        
-    }
-}
-
-function handleArrowKey(event) {
-    switch(event.key) {
-      case "ArrowUp":
-        console.log("Up arrow pressed");
-        // Add your up arrow handling code here
-        break;
-      case "ArrowDown":
-        console.log("Down arrow pressed");
-        // Add your down arrow handling code here
-        break;
-      case "ArrowLeft":
-        console.log("Left arrow pressed");
-        // Add your left arrow handling code here
-        break;
-      case "ArrowRight":
-        console.log("Right arrow pressed");
-        // Add your right arrow handling code here
-        break;
-    }
-  }
-
-  document.addEventListener('keydown', handleArrowKey);
-
 
 function getData(task) {
     return fetch('data.json').then(response => {
@@ -63,11 +31,11 @@ function getData(task) {
         return null;
     }).then(result => {
         if (result != null) {
-            initBoard(result, task);
+            let grid = result.tasks[task].grid;
+            createGrid(grid);
         } else {
             console.error("response is empty");
         }
     })
 }
 getData(0);
-
