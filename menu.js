713cc5f012b2b0654a8188f9306620dda13fffef
RadovanBorsig <radovan.borsig@gmailc.om>
2023-12-28 22:30:56 +0100

    update

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 let closeButton = document.getElementById("menuClose");
 let showButton = document.getElementById("menuOpen");
-let modal = document.getElementById("modalDiv");
+let modal = document.getElementById("modalDivI");
 
 closeButton.addEventListener("click", ()=>{
     modal.style.display = "none";
